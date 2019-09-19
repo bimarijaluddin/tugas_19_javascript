28d90c7292598e06efd9508a5410273e6a76b77d
bima rijaluddin <bimarijal@gmail.com>
2019-09-19 20:33:14 +0800

    Tugas 19 Javascript
    Tugas dari niomic

---
+++ tugas19.js
@@ -0,0 +1,20 @@
+function repeat1(){
+    var kata = "Semangat ...";
+    console.log(kata.repeat(3));
+}
+
+repeat1();
+
+
+function repeat2(){
+    var bintang = "*";
+    console.log(bintang.repeat(5));
+    console.log(bintang.repeat(25));
+    console.log(bintang.repeat(35));
+    console.log(bintang.repeat(55));
+    console.log(bintang.repeat(35));
+    console.log(bintang.repeat(25));
+    console.log(bintang.repeat(5));
+}
+
+repeat2();
